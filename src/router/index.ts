3095d7cad2cef82e53ec99119c5c6ce1a534277f
ioudan <2020.meng.dd>
2021-01-14 09:49:01 +0800

    全局组件 <Nav/>

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,41 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import Labels from '@/views/Labels.vue';
+import Money from '@/views/Money.vue';
+import Statistics from '@/views/Statistics.vue';
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Money',
+    component: Money
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/labels',
+    name: 'Labels',
+    component: Labels
+  },
+  {
+    path: '/money',
+    name: 'Money',
+    component: Money
+  },
+  {
+    path: '/statistics',
+    name: 'Statistics',
+    component: Statistics
   }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Labels.vue
@@ -0,0 +1,18 @@
+<template>
+<div>Labels
+
+  <Nav/>
+</div>
+</template>
+
+<script lang="ts">
+import Nav from '@/components/Nav.vue';
+export default {
+name: "Labels",
+  components: {Nav}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
