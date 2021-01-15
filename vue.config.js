76a11381000482c417913905324b619bfd1c7a43
ioudan <2020.meng.dd>
2021-01-15 09:57:16 +0800

    安装SVG优化器 svgo-loader 设置svgo-loader的配置文件 移除fill属性，去除svg自带的颜色

--- vue.config.js
+++ vue.config.js
@@ -1,45 +1,39 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 
-module.exports = {
-    lintOnSave: false
-}
-
 function resolve(dir) {
     return path.join(__dirname, '.', dir)
 }
-
 module.exports = {
     chainWebpack: config => {
-        config.module.rules.delete("svg"); //重点:删除默认配置中处理svg,
-        //const svgRule = config.module.rule('svg')
-        //svgRule.uses.clear()
+        // 清除svg默认配置对./src/icons文件夹的处理
         config.module
-            .rule('svg-sprite-loader')
+            .rule("svg")
+            .exclude
+            .add(resolve("src/assets/icons"))
+            .end()
+
+        // 添加新的rule处理./src/icons文件夹的svg文件
+        config.module
+            .rule("svg-sprite")
             .test(/\.svg$/)
             .include
-            .add(resolve('src/assets/icons')) //处理svg目录
+            .add(resolve("src/assets/icons"))
             .end()
-            .use('svg-sprite-loader')
-            .loader('svg-sprite-loader')
+            .use("svg-sprite-loader")
+            .loader("svg-sprite-loader")
             .options({
-                symbolId: 'icon-[name]'
+                symbolId: "icon-[name]"
             })
-    },
-    configureWebpack: () => ({
-        // resolve: {
-        //   alias: require('./alias.config').webpack
-        // }
-        // module: {
-        //   rules: [{
-        //     test: /\.svg$/,
-        //     use: [{
-        //       loader: "svg-sprite-loader",
-        //       options: {
-        //         symbolId: 'icon-[name]'
-        //       }
-        //     }]
-        //   }]
-        // }
-    })
+            .end()
+            .before("svg-sprite-loader")
+            .use("svgo-loader")
+            .loader("svgo-loader")
+            .options({
+                plugins: [
+                    {removeAttrs: {attrs: "path:fill"}}
+                ]
+            })
+            .end()
+    }
 }
