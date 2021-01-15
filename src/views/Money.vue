c9622a1a89b1fbdeca44591b3a88bb44e0349325
ioudan <2020.meng.dd>
2021-01-15 10:42:13 +0800

    记账页HTML第二版

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,42 +2,45 @@
   <div>
     <Layout>
       <div class="tags">
-          <ul class="tag">
-            <li>衣</li>
-            <li>食</li>
-            <li>住</li>
-            <li>行</li>
-          </ul>
-        <button class="tag-add">新增标签</button>
+        <ul class="tag">
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ul>
+        <div class="tagAdd">
+          <button>新增标签</button>
+        </div>
       </div>
-      <div class="notes">
+      <div>
         <label class="note">
+          <span class="noteName">备注</span>
           <input type="text">
         </label>
       </div>
-      <div class="types">
-        <ul>
-          <li>支出</li>
+      <div>
+        <ul class="type">
+          <li class="typeSelected">支出</li>
           <li>收入</li>
         </ul>
       </div>
-      <div class="number-pad">
-        <div class="output">100</div>
-        <div class="pad">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>删除</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-          <div>清空</div>
-          <div>7</div>
-          <div>8</div>
-          <div>9</div>
-          <div>OK</div>
-          <div>0</div>
-          <div>.</div>
+      <div class="numberPad">
+        <div class="numberOutput">100</div>
+        <div class="numberButtons">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>OK</button>
+          <button>0</button>
+          <button>.</button>
         </div>
 
       </div>
