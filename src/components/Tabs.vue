6235f5c8bc768b4be6a27907f1b0efe906e1a452
ioudan <2020.meng.dd>
2021-01-21 11:22:20 +0800

    抽离Tabs.vue，使用在Statistics.vue 里面

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,38 +1,42 @@
 <template>
-  <div>
-    <ul class="type">
-<!--      表驱动，所有的类 用表写出来-->
-      <li :class="{[classPrefix+'-item']:classPrefix ,typeSelected:value === '-'}" @click="selectType('-')"><span>支出</span></li>
-      <li :class="{[classPrefix+'-item']:classPrefix ,typeSelected:value === '+'}" @click="selectType('+')"><span>收入</span></li>
-    </ul>
-  </div>
+  <ul class="tabs">
+    <li v-for="item in dataSource" :key="item.value" @click="select(item)" :class="liClass(item)">{{ item.text }}</li>
+  </ul>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
 
+type DataSourceItem = {
+  text: string;
+  value: string;
+}
 @Component
-export default class Type extends Vue {
+export default class Tabs extends Vue {
+  @Prop({required: true, type: Array}) dataSource!: DataSourceItem;
+
   // 默认值'-'，只有value的值为undefined时才有，为''时也不会用默认的
-  @Prop({default:''}) readonly value!: string;
-  @Prop(String) classPrefix?: string;
+  @Prop({default: ''}) readonly value!: string;
+
   // ?问号   的意思是 可能不是undefined
   // !感叹号 的意思是 绝对不可能是undefined
+  @Prop(String) classPrefix?: string;
 
-  selectType(types: ('-' | '+')) {
-    if (['-', '+'].indexOf(types) === -1) {
-      throw new Error('类型不存在！');
-    }
-    this.$emit('update:value', types);
+  liClass(item: DataSourceItem) {
+    return {[this.classPrefix + '-tabs-item']: this.classPrefix, typeSelected: item.value === this.value};
+  }
+
+  select(item: DataSourceItem) {
+    this.$emit('update:value', item.value);
+
   }
 
 }
-
 </script>
 
 <style lang="scss" scoped>
-.type {
+.tabs {
   display: flex;
   background: #c4c4c4;
 
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>
-    <Layout>
-      <span>Statistics</span>
-    </Layout>
-  </div>
+  <Layout>
+<!--    <Type class-prefix="zzz3" :value.sync="yyy"/>-->
+    <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
+    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
+  {{type}}
+    <hr>
+    {{interval}}
+  </Layout>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Statistics',
-};
+import Type from '@/components/Money/Type.vue';
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
+
+@Component({
+  components: {Tabs, Type},
+
+})
+export default class Statistics extends Vue {
+  type = '-';
+  interval = 'day';
+  intervalList = [{text: '按天', value: 'day'}, {text: '按周', value: 'week'}, {text: '按月', value: 'month'}];
+  typeList = [{text: '支出', value: '-'}, {text: '收入', value: '+'}];
+}
 </script>
+<style scoped lang="scss">
+::v-deep .type-tabs-item {
+  background: #FFF;
+
+  &.typeSelected {
+    background: #C4C4C4;
+
+    &::after {
+      display: none;
+    }
+  }
+}
+
+</style>
